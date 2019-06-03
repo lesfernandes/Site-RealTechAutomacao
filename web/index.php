1e6f24fd5ee495f1ae45e6fd9933bfc4e0a62133
lesoaress <lemayara16@gmail.com>
2019-06-03 16:13:37 -0300

    media queries

--- web/index.php
+++ web/index.php
@@ -159,6 +159,97 @@
             </div>
             </div>
          </div>
+         <div class="service" id="servicesPhone">
+            <div class="container">
+               <div class="w3ls_banner_bottom_grids agileinfo_testimonial_grids">
+                  <div class="service-info">
+                     <h3 class="title" id="titleServices">SERVIÇOS</h3>
+                  </div>
+               </div>
+               <div class="row agile_gallery_grids">
+               <div class="col-lg-4 col-md-6 col-sm-6 col-6 agile_gallery_grid">
+                  <div class="hover ehover14" id="hoverServices">
+                     <a href="paineis.php">
+                        <img src="images/painel.jpg" alt=" " class="img-responsive" />
+                        <div class="overlay">
+                           <h4>MONTAGEM DE PAINÉIS E BANDEIJAMENTO</h4>
+                        </div>
+                     </a>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6 col-sm-6 col-6 agile_gallery_grid">
+                  <div class="hover ehover14" id="hoverServices">
+                     <a href="iluminacao.php">
+                        <img src="images/iluminacao.jpg" alt=" " class="img-responsive" />
+                        <div class="overlay">
+                           <h4>ILUMINAÇÃO INDUSTRIAL</h4>
+                        </div>
+                     </a>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6 col-sm-6 col-6 agile_gallery_grid">
+                  <div class="hover ehover14" id="hoverServices">
+                     <a href="postos.php">
+                        <img src="images/posto.jpg" alt=" " class="img-responsive" />
+                        <div class="overlay">
+                           <h4>MONTAGEM DE POSTO DE MÉDIA TENSÃO</h4>
+                        </div>
+                     </a>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6 col-sm-6 col-6 agile_gallery_grid grid1">
+                  <div class="hover ehover14" id="hoverServices">
+                     <a href="leitos.php">
+                        <img src="images/tubulacao.jpg" alt=" " class="img-responsive" />
+                        <div class="overlay">
+                           <h4>LEITOS E TUBULAÇÕES</h4>
+                        </div>
+                     </a>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6 col-sm-6 col-6 agile_gallery_grid grid1">
+                  <div class="hover ehover14" id="hoverServices">
+                     <a href="projetos.php">
+                        <img src="images/s3.jpg" alt=" " class="img-responsive" />
+                        <div class="overlay">
+                           <h4>PROJETOS ELÉTRICOS INDUSTRIAIS</h4>
+                        </div>
+                     </a>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6 col-sm-6 col-6 agile_gallery_grid grid1">
+                  <div class="hover ehover14" id="hoverServices">
+                     <a href="manutencoes.php">
+                        <img src="images/manutencao.jpg"/>
+                        <div class="overlay">
+                           <h4>MANUTENÇÕES PREVENTIVAS E CORRETIVAS</h4>
+                        </div>
+                     </a>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6 col-sm-6 col-6 agile_gallery_grid grid1">
+                  <div class="hover ehover14" id="hoverServices">
+                     <a href="spda.php">
+                        <img src="images/manutencao.jpg" alt=" " class="img-responsive" />
+                        <div class="overlay">
+                           <h4 id="spda">SPDA - SISTEMAS DE PROTEÇÃO CONTRA DESCARGAS ATMOSFÉRICAS</h4>
+                        </div>
+                     </a>
+                  </div>
+               </div>
+               <!--<div class="col-lg-4 col-md-6 col-sm-6 col-6 agile_gallery_grid grid1">
+                  <div class="hover ehover14" id="hoverServices">
+                     <a href="images/manutencao.jpg" class="lsb-preview wthree_p_grid" data-lsb-group="header">
+                        <img src="images/manutencao.jpg" alt=" " class="img-responsive" />
+                        <div class="overlay">
+                           <h4 id="spda">SPDA - SISTEMAS DE PROTEÇÃO CONTRA DESCARGAS ATMOSFÉRICAS</h4>
+                        </div>
+                     </a>
+                  </div>
+               </div>-->
+            </div>
+            </div>
+         </div>
       <!--//services--> 
       <!--clients-->
       
